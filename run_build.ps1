69b9536b87909599b7a673a0cda421e5c392336c
phucknguyen <phucknguyen@gmail.com>
2018-12-19 10:29:19 +0700

    Delete katalon project files not needed

--- run_build.ps1
+++ run_build.ps1
@@ -1,7 +1,5 @@
 # Kick off a build on circleci
 
-#curl -d '{json}' -H "Content-Type: application/json" https://circleci.com/api/v1.1/project/github/phucnguyen81/ci-samples?circle-token=8b9386241a753945fdc583d796f5ece55d861039
-
 $uri = 'https://circleci.com/api/v1.1/project/github/phucnguyen81/ci-samples?circle-token=8b9386241a753945fdc583d796f5ece55d861039'
 $headers = @{
   'Content-Type' = 'application/json'
