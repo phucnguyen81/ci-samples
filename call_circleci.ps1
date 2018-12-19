514b5f54cc4896d3422fd7373dbacb4fedee7c47
phucknguyen <phucknguyen@gmail.com>
2018-12-19 12:39:29 +0700

    Require url from build parameters

--- call_circleci.ps1
+++ call_circleci.ps1
@@ -1,7 +1,15 @@
 # Kick off a build on circleci
 
 $uri = 'https://circleci.com/api/v1.1/project/github/phucnguyen81/ci-samples?circle-token=8b9386241a753945fdc583d796f5ece55d861039'
+
 $headers = @{
   'Content-Type' = 'application/json'
 }
-Invoke-RestMethod -Uri $uri -Method Post -Headers $headers
+
+$body = @{
+  build_parameters: @{
+    DOWNLOAD_URL: 'https://phucknguyen-bucket.s3.amazonaws.com/ci-samples-2.zip'
+  }
+}
+
+Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
