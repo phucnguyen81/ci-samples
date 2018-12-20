e53a1ab43780cff055390fa45fcf4e620f99bcca
phucknguyen <phucknguyen@gmail.com>
2018-12-20 09:40:48 +0700

    Call with env vars

--- call_circleci.ps1
+++ call_circleci.ps1
@@ -25,6 +25,7 @@ $body = @{
   build_parameters = @{
     DOWNLOAD_URL = 'https://phucknguyen-bucket.s3.amazonaws.com/ci-samples-2.zip'
     COMMAND = 'katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
+    ENVIRONMENT_VARIABLES = ''
   }
 }
 
